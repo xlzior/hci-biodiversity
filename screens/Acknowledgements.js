fd311f9071df3d503d7289b12035f6ab17f39242
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2019-05-31 12:21:29 +0800

    Update Acknowledgements.js

--- screens/Acknowledgements.js
+++ screens/Acknowledgements.js
@@ -64,6 +64,23 @@ export default class Acknowledgements extends React.Component {
           <Text style={styles.leftTitle2}>App Development</Text>
           <Text style={justifiedDescription}>Lye Wen Jun</Text>
           <Text style={justifiedDescription}>Leonard Ong Rui Fu</Text>
+          <Text>{"\n"}</Text>
+
+          <Text style={justifiedDescription}>We would also like to acknowledge the key following sources of information for our writeups. We declare that no photographs from these sources have been used by us.</Text>
+          <Text style={justifiedDescription}>Chin, Jacqueline, and John Chin. John&Jacq's Garden. 2011. Accessed 2019. https://www.jaycjayc.com/.</Text>
+          <Text style={justifiedDescription}>Fern, Ken et al. "Useful Tropical Plants." Useful Tropical Plants. Accessed 2019. http://tropical.theferns.info/.</Text>
+          <Text style={justifiedDescription}>Flowers of India. Accessed 2019. http://www.flowersofindia.net/.</Text>
+          <Text style={justifiedDescription}>Master Gardener Program. 2019. Accessed 2019. https://wimastergardener.org/.</Text>
+          <Text style={justifiedDescription}>Missouri Botanical Garden. Accessed 2019. http://www.missouribotanicalgarden.org/.</Text>
+          <Text style={justifiedDescription}>Mound, Laurence A, Dom W Collins and Anne Hastings. Thysanoptera Britannica et Hibernica - Thrips of the British Isles. Lucidcentral.org, Identic Pty Ltd, Queensland, Australia. 2018. Accessed 2019. https://keys.lucidcentral.org</Text>
+          <Text style={justifiedDescription}>National Center for Biotechnology Information (NCBI). Accessed 2019. https://www.ncbi.nlm.nih.gov/.</Text>
+          <Text style={justifiedDescription}>National Library Board Singapore. eResources. 2019. Accessed 2019. http://eresources.nlb.gov.sg/.</Text>
+          <Text style={justifiedDescription}>National Parks Board (NParks). Accessed 2019. https://www.nparks.gov.sg/.</Text>
+          <Text style={justifiedDescription}>Plants For A Future. 2012. Accessed 2019. https://pfaf.org/user/Default.aspx.</Text>
+          <Text style={justifiedDescription}>Slik, Ferry et al. Plants of Southeast Asia. 2009. Accessed 2019. http://www.asianplant.net/.</Text>
+          <Text style={justifiedDescription}>Stuart, Godofredo U. StuartXchange. 2018. Accessed 2019. http://www.stuartxchange.org/.</Text>
+          <Text style={justifiedDescription}>Wikipedia. Accessed 2019. https://en.wikipedia.org/.</Text>
+
 
         </View>
       </NavigationBar>
